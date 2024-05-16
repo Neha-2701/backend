d491df6be259cd84a05b6252d07c0b73ab9d0ba1
neha-garg-27 <neha.garg@growthjockey.com>
2024-05-16 23:19:10 +0530

    remove /

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3001/", ""],
+    origin: ["http://localhost:3001", ""],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
