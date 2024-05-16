d01dec1c0b497c90f2546c3f13744958cfe32e97
neha-garg-27 <neha.garg@growthjockey.com>
2024-05-16 23:20:18 +0530

    changed to 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3001", ""],
+    origin: ["http://localhost:3000", ""],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
